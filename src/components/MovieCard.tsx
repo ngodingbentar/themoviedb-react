dacf0e0f5d3f1c9c78eae4548e218239d901def3
ngodingbentar <dewaqintoro@gmail.com>
2023-11-29 22:18:38 +0700

    Added: card

---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,28 @@
+import { IMovie } from "../types/main"
+
+const MovieCard = ({movie}: {movie: IMovie}) => {
+  const sliceOverview = () => {
+    if(movie.title.length < 15) return 150
+    if(movie.title.length >= 50) return 50
+    if (movie.title.length > 30 && movie.title.length < 40 ) return 80
+    if(movie.title.length > 40 && movie.title.length < 50 ) return 60
+    return 100
+  }
+  return (
+    <div className="bg-white shadow-lg rounded-xl overflow-hidden max-w-xs order-first lg:order-none h-[400px]">
+      <div>
+        <img src={`https://image.tmdb.org/t/p/w400${movie.backdrop_path}`} alt={movie.title} />
+      </div>
+      <div className="py-5 px-6 sm:px-8">
+        <h2 className="text-xl sm:text-2xl text-gray-800 font-semibold mb-3">{movie.title}</h2>
+        {movie.overview.length > 150 ? (
+          <p className="text-gray-500 leading-relaxed">{movie.overview.slice(0, sliceOverview())},...</p>
+        ) : (
+          <p className="text-gray-500 leading-relaxed">{movie.overview}</p>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default MovieCard
--- src/pages/movies.tsx
+++ src/pages/movies.tsx
@@ -1,17 +1,23 @@
 import { IMovie } from "../types/main"
 import { useGetMovies } from "../hooks/useMovies"
+import MovieCard from "../components/MovieCard"
 
 function MoviesPage() {
   const {data, isLoading} = useGetMovies()
 
   return (
     <div className='p-8'>
-      {isLoading && <div>Loading...</div>}
-      {!isLoading && data.map((movie: IMovie) => (
-        <div key={movie.id}>
-          {movie.title}
+      <div>
+        <div className="min-h-screen">
+          <div className="min-h-screen max-w-6xl mx-auto justify-center grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-8 gap-y-5 ">
+          {!isLoading && data.map((movie: IMovie) => (
+            <div key={movie.id}>
+              <MovieCard movie={movie} />
+            </div>
+          ))}
+          </div>
         </div>
-      ))}
+      </div>
     </div>
   )
 }
